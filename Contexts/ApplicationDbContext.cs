599de2bf4f1d5c88edc24eaa80efb56bb062a8a2
siva <sivannarayana.dokku@gmail.com>
2020-08-27 16:20:37 -0600

    What is Seeding? During development, we may need some sample data in our database to test the application. EF Core addresses this requirement by the Seed Functionality. It essentially means that we can push sample data to the database at defined entry events. In this scenario we will add default Roles and a default user to our database using this feature.

---
+++ Contexts/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using JwtAspNetCoreWebApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtAspNetCoreWebApi.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
